2d29c9fed6e1f61c5d1bd69d9ba12334df9208f9
Azimjon Abdurasulov <128313225+lifehackss@users.noreply.github.com>
2023-03-30 10:10:19 +0500

    Add files via upload

---
+++ lesson_8.py
@@ -0,0 +1,21 @@
+# Azimjon Abdurasulov
+# Python data-types
+
+number = 10 # int
+float_number = 1.5 # float
+string = "Azimjon" # str ''
+bools = True # bool
+lists = [1, 2, 3, "Salom", 1.5] # list
+tuples = (1, "Salom") # tuple
+
+print(number)
+print(float_number)
+print(string)
+print(bools)
+print(lists)
+print(tuples)
+
+
+# Home work!
+# print() fungsiyasi orqali har bir o'zgaruvchilarni data-type larini ko'rib chiqing
+print(type(number))
