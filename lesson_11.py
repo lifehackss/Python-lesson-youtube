ed643c11bdbef8384b8508d6b9a2777bb9302984
Azimjon Abdurasulov <128313225+lifehackss@users.noreply.github.com>
2023-04-03 06:40:05 +0500

    Add files via upload

---
+++ lesson_11.py
@@ -0,0 +1,28 @@
+# Azimjon Abdurasulov
+# Arithmetic operators
+
+a = 5
+b = 2
+print(a + b) # Addition - Qo'shimcha
+print(a - b) # Subtraction - Ayirish
+print(a * b) # Division - Bo'lim
+print(a / b) # Multiplication - Ko'paytirish
+print(a % b) # Modulus - Modul
+print(a ** b) # Exponentiation - Ko'tarilish
+print(a // b) # Floor division - Qavat bo'limi
+
+
+
+
+
+
+
+
+# Home work!
+"""
+Har bir o'rganilgan operator uchun alohida misol yozing va bu operatorlar bilan ko'proq mashiq qiling.
+
+misol:
+Hozirgi yilidan tug'ilgan yilingizni ayirish orqali sizning yoshingiz nechada ekanini print() yordamida chop eting!.
+
+"""
